1c40f22249095bc2dd43a46398ba0cb2ef5d4cf5
dalexez <dalexez@gmail.com>
2022-04-26 11:56:38 -0500

    Creacion del servidor node

---
+++ app.js
@@ -0,0 +1,20 @@
+//Usando objeto express
+const express = require('express')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
+
+//App de Express
+const app = express()
+
+//Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+//Path inicial, respondera a la url localhost:3000
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+//Con esto inicializamos esta app
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
